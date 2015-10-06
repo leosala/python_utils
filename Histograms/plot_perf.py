77816576575885b7d620553d12e97a742b690784
Sala Leonardo <Leonardo.Sala@psi.ch>
2015-10-06 21:28:36 +0200

    readme

--- Histograms/plot_perf.py
+++ Histograms/plot_perf.py
@@ -8,9 +8,10 @@ print df
 del df["std"]
 
 plt.figure()
+plt.title("100M data points, uint16")
 plt.xlabel("#bins")
 plt.ylabel("seconds")
 for i in ["std numpy", "dig / bincount", "cython"]:
-    plt.plot(df[df["name"] == i]["bins"], df[df["name"] == i]["mean"], label=i)
+    plt.plot(df[df["name"] == i]["bins"], df[df["name"] == i]["mean"], label=i, linewidth=2)
 plt.legend(loc='best')
 plt.show()
