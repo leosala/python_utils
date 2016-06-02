45af8b9bf05906c87f93a45a1c39c99f01957402
Leonardo Sala <leonardo.sala@psi.ch>
2016-06-02 17:09:47 +0200

    removed pandas normalization

--- pandas/read_iftop.py
+++ pandas/read_iftop.py
@@ -6,10 +6,13 @@ def normalize_file(fname):
     """
     Some basic and horrible data wrangling is needed before using pandas
     """
+
+    # grep -v "#"  | grep -v "\---" | grep -v "rate" | grep -v "==" | grep -v "Cumulative" | grep -v '^$' | sed "s=^  ==g" | sed "s:^ ::g" | sed "s=  *=:=g"
     cmd = 'grep -v "#" %s |' % fname
     cmd += ' grep -v "\---" | grep -v "rate" | grep -v "==" | '
     cmd += ' grep -v "Cumulative" | grep -v "^$" | sed "s=^   ==g" |'
     cmd += ' sed "s=  *=:=g" > %s_norm.log' % fname
+    print cmd
     retcode = subprocess.call(cmd, shell=True)
     if retcode != 0:
         print "[ERROR] Something went wrong in the IFTOP file normalization"
@@ -18,8 +21,14 @@ def normalize_file(fname):
 
 
 def convert_units(x):
+    """MB/s"""
     try:
-        return float(x.replace("MB", "000000").replace("KB", "000").replace("B", "")) / 1000000
+        if x[-2:] == "MB":
+            return float(x.replace("MB", "")) 
+        elif x[-2:] == "KB":
+            return float(x.replace("KB", "")) / 1000
+        elif x[-2:] == "B":
+            return float(x.replace("B", "")) / 1000000
     except:
         return -1
 
@@ -28,10 +37,12 @@ def read_iftop(fname):
     """
     loading normalized iftop output
     """
-    norm_fname = normalize_file(fname)
+    #norm_fname = normalize_file(fname)
+    norm_fname = fname
     if norm_fname is None:
         return -1
-    
+
+    # add case for extra columns (port)
     df = pd.read_csv(norm_fname, sep=':', skiprows=1,
                      names=["id", "host", "dir", "2s", "10s", "40s", "cum"],
                      converters={"2s": convert_units, "10s": convert_units, "40s": convert_units},
@@ -44,5 +55,5 @@ def read_iftop(fname):
     cols = df.columns.tolist()
     cols = cols[:3] + [cols[-1], ] + cols[3:-2]
     df = df[cols]
-    os.unlink(norm_fname)
+    #os.unlink(norm_fname)
     return df
