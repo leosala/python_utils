b46ea49041151ef683758b3952989cb2d7f9a053
Heiner Billich <Heiner.Billich@psi.ch>
2016-06-14 17:04:58 +0200

    wip

--- udp/socket_replay.py
+++ udp/socket_replay.py
@@ -8,18 +8,18 @@ import sys
 import numpy as np
 
 
-def run_replay(filename, ip, port):
+def run_replay(filename, ip, port, sleep_time=0.01):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print(s.connect((ip, int(port))))
     c = 0
     with open(filename, 'rb') as f:
-        for data in iter(lambda: f.read(8214), ''):
+        for data in iter(lambda: f.read(6080), ''):
         #for data in f.readlines():
             c += 1
             try:
                 s.sendto(data, (ip, int(port)))
                 print("sent", c) 
-                sleep(.1)
+                sleep(sleep_time)
             except:
                 print(sys.exc_info()[1])
                 s.close()
