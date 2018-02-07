8d52535731a1d385cd44839896a13831d7296c7c
Leonardo Sala <leonardo.sala@psi.ch>
2018-02-07 09:36:29 +0100

    wip

--- udp/run_multiple_dumpers.sh
+++ udp/run_multiple_dumpers.sh
@@ -1,38 +1,16 @@
 #!/bin/bash
 
-#taskset -c 19 
-python socket_capture.py 10.0.0.100 2000 -o /scratch/leo/10.0.0.100_2000.dat & 
-#taskset -c 20 
-python socket_capture.py 10.0.0.100 2001 -o /scratch/leo/10.0.0.100_2001.dat &
-#taskset -c 23 
-python socket_capture.py 10.0.0.100 2002 -o /scratch/leo/10.0.0.100_2002.dat&
-#taskset -c 24 
-python socket_capture.py 10.0.0.100 2003 -o /scratch/leo/10.0.0.100_2003.dat &
+IPS="10.30.20.5"
+PORTS=`seq 50000 50008`
+PIDS=""
+OUTDIR="/tmp"
 
-#taskset -c 6 
-python socket_capture.py 10.4.0.100 2004 -o /scratch/leo/10.4.0.100_2004.dat &
-#taskset -c 7 
-python socket_capture.py 10.4.0.100 2005 -o /scratch/leo/10.4.0.100_2005.dat &
-#taskset -c 10 
-python socket_capture.py 10.4.0.100 2006 -o /scratch/leo/10.4.0.100_2006.dat &
-#taskset -c 11 
-python socket_capture.py 10.4.0.100 2007 -o /scratch/leo/10.4.0.100_2007.dat &
+for ip in $IPS; do
+    for port in $PORTS; do
+	python socket_capture.py $ip $port -o /$OUTDIR/$ip_$port.dat &
+	PIDS="$! $PIDS"
+    done
+done
 
-#taskset -c 22 
-python socket_capture.py 10.0.0.100 2008 -o /scratch/leo/10.0.0.100_2008.dat & 
-#taskset -c 23 
-python socket_capture.py 10.0.0.100 2009  -o /scratch/leo/10.0.0.100_2009.dat &
-#taskset -c 26 
-python socket_capture.py 10.0.0.100 2010  -o /scratch/leo/10.0.0.100_2010.dat &
-#taskset -c 28 
-python socket_capture.py 10.0.0.100 2011  -o /scratch/leo/10.0.0.100_2011.dat &
-
-#taskset -c 8 
-python socket_capture.py 10.4.0.100 2012 -o /scratch/leo/10.4.0.100_2012.dat &
-#taskset -c 9 
-python socket_capture.py 10.4.0.100 2013 -o /scratch/leo/10.4.0.100_2013.dat &
-#taskset -c 12 
-python socket_capture.py 10.4.0.100 2014 -o /scratch/leo/10.4.0.100_2014.dat &
-#taskset -c 13 
-python socket_capture.py 10.4.0.100 2015 -o /scratch/leo/10.4.0.100_2015.dat &
-
+sleep 1
+echo "After capture is completed, execute kill -9 "$PIDS
