cbb7a2ff4c62d36f797962a1e1928bbfaeb98e6f
Leonardo Sala <leonardo.sala@psi.ch>
2018-07-10 10:10:03 +0200

    wip

--- hdf5/compression_tests.py
+++ hdf5/compression_tests.py
@@ -8,7 +8,7 @@ Created on Fri Sep 23 11:18:47 2016
 import numpy as np
 import tables
 from contextlib import closing
-from time import time
+from time import time, sleep
 import random
 import os
 import h5py
@@ -22,8 +22,10 @@ import scipy.stats
 import pickle
 
 
-label = "test"
-dataset = "jungfrau/data"
+gain_file = "/sf/alvra/config/jungfrau/jungfrau_4p5_gaincorrections_v0.h5"
+pede_file = "/sf/alvra/data/p17245/res/pedestal_20180703_1403_res.h5"
+#label = "test"
+dataset = "data/JF4.5M/data"
 n_tries = 1
 data = None
 #dest_dir = "/tmp/"
@@ -33,18 +35,18 @@ tot_size = size[0] * size[1]
 px_n = size[0] * size[1]
 zeros_perc = 0
 
-samples = ["orig", "zlib", "lzo", "blosc:lz4",  "blosc:lz4hc", 'blosc:snappy']
+samples = ["orig", "zlib", "lzo", "blosc:lz4", "blosc:lz4_9", "blosc:lz4hc", 'blosc:snappy']
 
 data = np.random.randint(0, 2**16, size=[100, 1024, 1024])
 non_zeros = int((1 - zeros_perc) * tot_size)
 
 
-def create_file(fname, data, complib, complevel):
+def create_file(fname, data, complib, complevel, bitshuffle=False):
     FILTERS = None
     if complib != "orig":
-        bitshuffle = False
-        if complib.find("blosc") != -1:
-            bitshuffle = True
+        if complib.find("blosc") == -1:
+            bitshuffle = False
+            print("Bitshuffle only enabled for BLOSC")
         FILTERS = tables.Filters(complib=complib, complevel=complevel, bitshuffle=bitshuffle)
         with closing(tables.open_file(fname, mode='w', filters=FILTERS)) as hdf:
             hdf.create_carray('/', 'array', obj=data)
@@ -52,21 +54,6 @@ def create_file(fname, data, complib, complevel):
         with closing(tables.open_file(fname, mode='w', )) as hdf:
             hdf.create_carray('/', 'array', obj=data)
             
-            
-def create_jf_images(data, n_img, zeros_perc, size, mode=0):
-    for i in range(0, n_img):
-        if mode == 0:
-            rand = np.random.random_integers(2000, 45000, size=int(len(x)))
-        elif mode == 1:
-            rand = np.random.random_integers(2000, 65000, size=int(len(x)))
-        elif mode == 2:
-            rand1 = np.random.random_integers(2000, 16000, size=int(14 * len(x) / 43))
-            rand2 = np.random.random_integers(18000, 32000, size=int(14. * len(x) / 43))
-            rand3 = np.random.random_integers(50000, 65000, size=int(15. * len(x) / 43))
-            rand = np.random.choice(np.append(rand1, np.append(rand2, rand3)),
-                                    int((1 - zeros_perc) * size[0] * size[1]))
-        data[i].reshape(tot_size)[x] = rand
-
 
 def read_file(fname, dset="/array", chunk=-1):
     f = tables.open_file(fname)
@@ -77,14 +64,24 @@ def read_file(fname, dset="/array", chunk=-1):
         d = data[i][:]
         times.append(time() - start)
     ntimes = np.array(times)
+    f.close()
     return [ntimes.sum(), ntimes.mean(), ntimes.std()]
 
 
-def write_file(fname, data, complib, complevel, chunk=-1):
+def write_file(fname, data, complib, complevel, bitshuffle=False, chunk=-1):
+    
     FILTERS = None
     times = []
+
+    if complib.find("_") != -1:
+        print(complib.split("_"))
+        complib, complevel = complib.split("_")
+        complevel = int(complevel)
     if complib != "orig":
-        FILTERS = tables.Filters(complib=complib, complevel=complevel)
+        if complib.find("blosc") == -1 and bitshuffle:
+            bitshuffle = False
+            print("Bitshuffle only enabled for BLOSC")
+        FILTERS = tables.Filters(complib=complib, complevel=complevel, bitshuffle=bitshuffle)
         with closing(tables.open_file(fname, mode='w', filters=FILTERS)) as hdf:
             table = hdf.create_carray('/', 'array', tables.Atom.from_type(data.dtype.name), shape=data.shape)
             for i in range(data.shape[0]):
@@ -92,6 +89,8 @@ def write_file(fname, data, complib, complevel, chunk=-1):
                 table[i] = data[i][:]
                 table.flush()
                 times.append(time() - start)
+                #print(time() - start)
+        hdf.close()
     else:
         with closing(tables.open_file(fname, mode='w', )) as hdf:
             table = hdf.create_carray('/', 'array', tables.Atom.from_type(data.dtype.name), shape=data.shape)
@@ -100,8 +99,9 @@ def write_file(fname, data, complib, complevel, chunk=-1):
                 table[i] = data[i][:]
                 table.flush()
                 times.append(time() - start)
-    hdf.close()
+        hdf.close()
     ntimes = np.array(times)
+    print([ntimes.sum(), ntimes.mean(), ntimes.std()])
     return [ntimes.sum(), ntimes.mean(), ntimes.std()]
 
 
@@ -109,30 +109,45 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Do write / read performance test on HDF5 files and compression')
     parser.add_argument('files', type=str, nargs='+',
                         help='existing HDF5 files (if none, a random one will be created)')
-    parser.add_argument('-n', type=int, default=10,
+    parser.add_argument('-n', type=int, default=100,
                         help='number of events to analyze (default=10)')
     parser.add_argument('--outdir', '-o', type=str, default="/tmp",
                         help='where to write temporary files')
-    parser.add_argument('--convert', '-c', type=bool, default=False,
+    parser.add_argument('--convert', '-c', action="store_true",
                         help="create also the converted file")
     parser.add_argument('--zerosuppress', '-z', type=float, default=-99,
                         help="Put to zero values smaller than this. Onlu apply if convert is enabled")
+    parser.add_argument('--label', '-l', type=str, default="test",
+                        help='label for output file')
+    parser.add_argument('--bitshuffle', '-b', action="store_true",
+                        help="create also the converted file")
 
     args = parser.parse_args()
-    print(args.files, args.outdir)
+    label = args.label
+    if args.convert:
+        label += "-conv"
+    if args.zerosuppress != -99:
+        label += "-z{:.2f}".format(args.zerosuppress)
+    if args.bitshuffle:
+        label += "-bitshuffle"
+        
     if args.files != []:
         n_tries = len(args.files)
 
-    corrections_file = ""
+    print(args)
+    #corrections_file = ""
     if args.convert:
-        cf = h5py.File(corrections_file, "r")
-        G = cf["gains"]
-        P = cf["pedes"]
+        gf = h5py.File(gain_file, "r")
+        G = gf["gains"][:]
+        cf = h5py.File(pede_file, "r")
+        P = cf["gains"][:]
         
     sizes = {}
     ratios = {}
     times = {}
     reads = {}
+    n_tries = 2
+
     for s in samples:
         sizes[s] = np.zeros((n_tries, 3))
         ratios[s] = np.zeros((n_tries, 3))
@@ -147,41 +162,43 @@ if __name__ == "__main__":
             create_jf_images(data, args.n, zeros_perc, size, mode=0)
             zeros_perc = 1 - np.count_nonzero(data[0]) / float(data[0].shape[0] * data[0].shape[1])
         else:
+            print("Opening file", args.files[t])
             data = h5py.File(args.files[t])[dataset][:args.n][:]
             if args.convert:
+                data2 = np.ndarray(shape=data.shape, dtype=np.float16)
                 for i, d in enumerate(data):
-                    t = ju.apply_gain_pede(d, G=G, P=P)
+                    temp = ju.apply_gain_pede(d, G=G, P=P)
                     if args.zerosuppress != -99:
-                        data[i] = t[t < args.zerosuppress] = 0
+                        temp[temp < args.zerosuppress] = 0
+			
+                    data2[i][:] = temp[:]
             
         time_t = {}
 
         for s in samples:
-            fname = os.path.join(args.outdir, "test_" + s + ".h5")
-            start = time()
-            create_file(fname, data, s, 5)
-            time_t[s] = time() - start
-
+            print(s, t)
+            fname = os.path.join(args.outdir, "test_" + s + label +".h5").replace(":", "_")
+            
+            if args.convert:
+                print("%d %f" % (data[0,0,0], data2[0,0,0]))
+                times[s][t] = write_file(fname, data2, s, 5, args.bitshuffle)
+            else:
+                times[s][t] = write_file(fname, data, s, 5, args.bitshuffle)
+            reads[s][t] = read_file(fname)
             sizes[s][t] = float(os.stat(fname).st_size) / (1000. * 1000.)
+            print(fname, os.stat(fname).st_size)
             ratios[s][t] = sizes["orig"][t] / sizes[s][t]
-            times[s][:] = write_file(fname, data, s, 5)
-            reads[s][:] = read_file(fname)
-            os.remove(fname)
 
-    print(times)
+            print("Removing ", fname)
+            os.remove(fname)
+            #sleep(2)
+        print(sizes)
+    #print(times)
     h = "| Metric |"
     for s in samples:
         h += " %s |" % s
 
     print(h)
-
-    for s in samples:
-        write_ratios = [x[0] / times['orig'][i][0] for i, x in enumerate(times[s])]
-        print(s, write_ratios, times[s][0][0], times['orig'][0][0])
-        print(s, scipy.stats.mstats.gmean(write_ratios))
-
-    outf = open(label + ".pkl", "wb")
-    pickle.dump({"files":args.files, "sizes":sizes, "writes":times, "reads":reads}, outf)
 
     line = "| SIZE |"
     plist = {}
@@ -211,5 +228,11 @@ if __name__ == "__main__":
 #        line += " %.2f +- %.2f |" % (100 * reads[s][0], 100 * reads[s][1])
 #    print(line)
 
+    for s in samples:
+        write_ratios = [x[0] / times['orig'][i][0] for i, x in enumerate(times[s])]
 
-    print(times)
+    outf = open(label + ".pkl", "wb")
+    pickle.dump({"files":args.files, "sizes":sizes, "writes":times, "reads":reads}, outf)
+
+
+    print(sizes)
